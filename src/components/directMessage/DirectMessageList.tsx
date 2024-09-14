4292b8efbff1545711dca1d38045da687d3cc645
Nika Wuladze <126957977+GeoLine02@users.noreply.github.com>
2024-09-15 00:12:38 +0400

    feat: dynamic friends page, general styling

---
+++ src/components/directMessage/DirectMessageList.tsx
@@ -0,0 +1,17 @@
+import DirectMessage from "./DirectMessage";
+
+const DirectMessagesList = () => {
+  return (
+    <section>
+      <DirectMessage />
+      <DirectMessage />
+      <DirectMessage />
+      <DirectMessage />
+      <DirectMessage />
+      <DirectMessage />
+      <DirectMessage />
+    </section>
+  );
+};
+
+export default DirectMessagesList;
--- src/components/friends/FriendList.tsx
+++ src/components/friends/FriendList.tsx
@@ -1,5 +1,14 @@
+import { people } from "@/mock/people";
+import Friend from "./Friend";
+
 const FriendList = () => {
-  return <div>FriendList</div>;
+  return (
+    <div>
+      {people.map((member) => (
+        <Friend userID={member} key={member} />
+      ))}
+    </div>
+  );
 };
 
 export default FriendList;
